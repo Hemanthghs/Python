30856affaf82cb9122aff921e835b8a7a44d7461
Hemanthsai G <ghshemanth@gmail.com>
2021-05-09 00:14:21 +0530

    Delete sub.py

--- sub.py
+++
@@ -1,5 +0,0 @@
-import random
-lang_list=['c','cpp','py','swift','jl','java','js','go','rb','NET','txt','lsp']
-
-for i in range(1,1001):
-    print("submission"+str(i)+"."+lang_list[random.randint(0,11)]+"\n")
