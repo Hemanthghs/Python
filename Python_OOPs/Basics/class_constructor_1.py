4ba3d3530c6da09d88bdbbb48c54db88132c1a3c
Hemanthsai G <ghshemanth@gmail.com>
2021-05-14 22:28:34 +0530

    Add files via upload

---
+++ Python_OOPs/Basics/class_constructor_1.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self,name):
+        self.name = name
+        print(name)
+    def bark():
+        print("Bark")
+    def run():
+        print("running")
+
+d1 = Dog("Sparky")
+print(d1.name)
+d2 = Dog("Jack")
+print(d2.name)
---
+++ class_methods_1.py
@@ -0,0 +1,11 @@
+class Math:
+    def add_one(self,x):
+        return x+1;
+    def add_two(self,x):
+        return x+2;
+    
+obj1 = Math()
+
+print(obj1.add_one(10))
+
+print(obj1.add_two(10))
